8b539ea80bbe84717ee991d9bb494b5dd31a6773
Hoang Long <hoanglong.cmb@gmail.com>
2024-03-17 02:07:09 +0700

    add cart config

---
+++ Org_DAL/Configurations/CartConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Org_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Org_DAL.Configurations
+{
+    public class CartConfiguration : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+           builder.HasKey(x => x.Id);
+            builder.HasOne(p => p.User).WithOne(p => p.Cart).HasConstraintName("FK_Cart_User");
+            
+        }
+    }
+}
