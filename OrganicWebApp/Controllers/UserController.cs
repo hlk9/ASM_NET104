dfb30c99806e06b70178bb74e5b127ce00e57d24
Hoang Long <hoanglong.cmb@gmail.com>
2024-03-21 21:52:18 +0700

    add user controller

---
+++ OrganicWebApp/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Org_DAL.Models;
+using Org_DAL.Repository;
+
+namespace OrganicWebApp.Controllers
+{
+    public class UserController:Controller
+    {
+        private readonly UserRepository _repos;
+
+        public UserController()
+        {
+            _repos = new UserRepository();
+        }
+
+        public IActionResult Index()
+        {
+
+            ICollection<User> users = _repos.GetAll();
+
+            return View(users);
+        }
+       
+
+    }
+}
