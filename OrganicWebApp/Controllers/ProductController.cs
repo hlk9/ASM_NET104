89c666b424ccbe93745800851817290867f68884
Hoang Long <hoanglong.cmb@gmail.com>
2024-03-21 21:52:10 +0700

    add product controller

---
+++ OrganicWebApp/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Org_DAL.Models;
+using Org_DAL.Repository;
+
+namespace OrganicWebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductDetailRepository _repos = new ProductDetailRepository();
+        public IActionResult Index()
+        {
+            IEnumerable<ProductDetail> products = new List<ProductDetail>();
+            products  = _repos.GetAll();
+            return View(products);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ProductDetail obj)
+        {
+            //if(ModelState.IsValid)
+           // {
+                _repos.Add(obj);
+                return RedirectToAction("Index");
+          //  }
+
+            //return View(obj);
+        }
+
+    }
+}
