2cceea6458f7e70adb6d9f4bc24670dde298866d
Hoang Long <hoanglong.cmb@gmail.com>
2024-03-22 00:25:03 +0700

    update product controller

--- OrganicWebApp/Controllers/ProductController.cs
+++ OrganicWebApp/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Org_DAL.Models;
 using Org_DAL.Repository;
 
@@ -6,10 +7,11 @@ namespace OrganicWebApp.Controllers
 {
     public class ProductController : Controller
     {
-        private readonly ProductDetailRepository _repos = new ProductDetailRepository();
+        private readonly ProductRepository _repos = new ProductRepository();
+        public readonly CategoryRepository _categories = new CategoryRepository();
         public IActionResult Index()
         {
-            IEnumerable<ProductDetail> products = new List<ProductDetail>();
+            IEnumerable<Product> products = new List<Product>();
             products  = _repos.GetAll();
             return View(products);
         }
@@ -17,12 +19,14 @@ namespace OrganicWebApp.Controllers
 
         public IActionResult Create()
         {
+            var listCate = _categories.GetAll();
+            ViewBag.CategoryList = new SelectList(listCate,"Id", "CategoryName");
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(ProductDetail obj)
+        public IActionResult Create(Product obj)
         {
             //if(ModelState.IsValid)
            // {
@@ -31,6 +35,44 @@ namespace OrganicWebApp.Controllers
           //  }
 
             //return View(obj);
+        }
+
+
+        public IActionResult Edit(int Id) 
+        {
+
+            var listCate = _categories.GetAll();
+            ViewBag.CategoryList = new SelectList(listCate, "Id", "CategoryName");
+            var product = _repos.GetById(Id);
+            return View(product);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            _repos.Update(obj);
+
+            return RedirectToAction("Index");
+
+        }
+
+
+        public IActionResult Delete(int Id)
+        {           
+            _repos.Delete(Id);
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Detail(int Id)
+        {
+
+            var listCate = _categories.GetAll();
+            ViewBag.CategoryList = new SelectList(listCate, "Id", "CategoryName");
+            var product = _repos.GetById(Id);
+            return View(product);
+
         }
 
     }
