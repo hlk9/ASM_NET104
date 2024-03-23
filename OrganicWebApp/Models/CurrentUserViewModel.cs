b012030ded128b4155e21ac18241220e995dd646
Hoang Long <hoanglong.cmb@gmail.com>
2024-03-23 23:08:00 +0700

    add viewmodel for user

---
+++ OrganicWebApp/Models/CurrentUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OrganicWebApp.Models
+{
+    public class CurrentUserViewModel
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
