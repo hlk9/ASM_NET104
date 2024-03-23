54688da0cf72b2031335ec98f60588e4c8bdb4d8
Hoang Long <hoanglong.cmb@gmail.com>
2024-03-23 23:07:10 +0700

    update viewbag

--- OrganicWebApp/Controllers/HomeController.cs
+++ OrganicWebApp/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Org_DAL.Models;
+using Org_DAL.Repository;
 using OrganicWebApp.Models;
 using System.Diagnostics;
 
@@ -7,6 +10,7 @@ namespace OrganicWebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly CategoryRepository _categoryRepository = new CategoryRepository();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -15,6 +19,9 @@ namespace OrganicWebApp.Controllers
 
         public IActionResult Index()
         {
+            List<Category> categories = _categoryRepository.GetAll().ToList();
+
+            ViewBag.ListCategory = new SelectList(categories, "Id", "CategoryName");
             return View();
         }
 
